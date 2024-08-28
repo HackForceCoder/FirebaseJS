d24f9ceb8f0a48f7aa6d37e6b28f0b9a2f6d79a3
HackForceCoder <milanalba@outlook.com>
2024-08-28 07:35:23 -0600

    updates

--- App.tsx
+++ App.tsx
@@ -1,11 +1,97 @@
+import { initializeApp } from "firebase/app";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  initializeAuth,
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  UserCredential,
+  getReactNativePersistence
+} from 'firebase/auth';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState } from "react";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDzL8z5UoBwNVF8cSLdUkV0ch4NvrEWvao",
+  authDomain: "test-c9e08.firebaseapp.com",
+  projectId: "test-c9e08",
+  storageBucket: "test-c9e08.appspot.com",
+  messagingSenderId: "565828800306",
+  appId: "1:565828800306:web:46a8d4be7ca2865bef512b"
+};
+
+const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app); // Sin persistencia en sesiones
+const auth = initializeAuth(app, { persistence: getReactNativePersistence(ReactNativeAsyncStorage) });
 
 export default function App() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <TextInput
+        placeholder="email"
+        onChangeText={text => {
+          setEmail(text);
+        }}
+      />
+
+      <TextInput
+        placeholder="password"
+        secureTextEntry={true}
+        onChangeText={text => {
+          setPassword(text);
+        }}
+      />
+
+      <Button
+        title="SIGN UP"
+        onPress={() => {
+          createUserWithEmailAndPassword(auth, email, password).then(
+            (userCredential: UserCredential) => {
+              alert("USER SIGNED UP SUCCESFULLY!\n" + userCredential.user.email);
+              console.log("USER SIGNED UP SUCCESFULLY!\n", userCredential.user.email);
+            }).catch((error: any) => {
+              if (error.code == "auth/missing-password") {
+                alert("Password is missing or invalid. Try again");
+              }
+              console.log("ERROR:", error.code, "\n", error.message)
+            });
+        }}
+      />
+
+      <Button
+        title="LOG IN"
+        onPress={() => {
+          signInWithEmailAndPassword(auth, email, password).then(
+            (userCredential: UserCredential) => {
+              alert("LOGGED IN SUCCESSFULLY!\n" + userCredential.user.email);
+              console.log("LOGGED IN SUCCESSFULLY!\n", userCredential.user.email);
+            }).catch((error: any) => {
+              if (error.code == "auth/missing-password") {
+                alert("Password is missing or invalid. Try again");
+              }
+              console.log("ERROR:", error.code, "\n", error.message)
+            });
+        }}
+      />
+
+      <Button
+        title="LOG OUT"
+        onPress={() => {
+          auth.signOut().then(() => {
+            alert("LOGGED OUT SUCCESSFULLY!");
+            console.log("LOGGED OUT SUCCESSFULLY!");
+          });
+        }}
+      />
+
     </View>
   );
 }
@@ -18,3 +104,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
